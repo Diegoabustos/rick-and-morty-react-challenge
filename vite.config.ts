3d6ae7f4f03d87e0d29b2e7d115bba40a5abc125
Diegoabustos <balorcode22@gmail.com>
2023-09-10 21:20:00 -0600

    feat: 🎸 add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/Diegoabustos/rick-and-morty-react-challenge/",
   plugins: [react()],
   server: {
     host: 'localhost',
